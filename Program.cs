d41ea6bccb8210d4d172405daf0a17ddef1a3eb4
Huawei Wang <huaweiwang@Huaweis-MacBook-Pro.local>
2020-03-13 14:52:53 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace dotnetcore_projects
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var array = new JArray {"Manual text", new DateTime(2000, 5, 23)};
+
+            var o = new JObject {["MyArray"] = array};
+
+            var json = o.ToString();
+            
+            Console.WriteLine(json);
+        }
+    }
+}
